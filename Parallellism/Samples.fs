35fcc8a59b04b0997b2aa20d53060720489f7003
Eric Bäckhage <eric.backhage@gmail.com>
2017-06-14 21:19:59 +0200

    Added sequential vs parallell site fetcher sample.

---
+++ Parallellism/Samples.fs
@@ -0,0 +1,24 @@
+﻿namespace Parallellism
+
+open System
+open System.IO
+open System.Net
+
+module samples =
+    let fetchUrl url =
+        let req = WebRequest.Create(Uri(url))
+        use resp = req.GetResponse()
+        use stream = resp.GetResponseStream()
+        use reader = new IO.StreamReader(stream)
+        let html = reader.ReadToEnd()
+        printfn "Finished downloading %s" url
+
+    let fetchUrlAsync url =
+        async {
+            let req = WebRequest.Create(Uri(url))
+            use! resp = req.AsyncGetResponse()
+            use stream = resp.GetResponseStream()
+            use reader = new IO.StreamReader(stream)
+            let html = reader.ReadToEnd()
+            printfn "Finshed downloading %s" url
+        }
--- Parallellism/Script.fsx
+++ Parallellism/Script.fsx
@@ -1,8 +1,22 @@
-﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
-// for more guidance on F# programming.
-
-#load "Library1.fs"
+﻿#load "Samples.fs"
 open Parallellism
 
-// Define your library scripting code here
+let sites = ["http://www.google.com";
+             "http://www.yahoo.com";
+             "http://www.microsoft.com";
+             "http://www.amazon.com";
+             "http://www.bing.com"]
 
+// Real: 00:00:02.636, CPU: 00:00:00.093, GC gen0: 1, gen1: 1, gen2: 0
+#time
+sites
+|> List.map samples.fetchUrl
+#time
+
+// Real: 00:00:00.378, CPU: 00:00:00.031, GC gen0: 0, gen1: 0, gen2: 0 
+#time
+sites
+|> List.map samples.fetchUrlAsync
+|> Async.Parallel
+|> Async.RunSynchronously
+#time
