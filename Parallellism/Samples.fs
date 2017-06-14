04baa78e6fa2985f0c72d7427c73059f063896e9
Eric Bäckhage <eric.backhage@gmail.com>
2017-06-14 21:44:12 +0200

    Added info comment

--- Parallellism/Samples.fs
+++ Parallellism/Samples.fs
@@ -5,6 +5,9 @@ open System.IO
 open System.Net
 
 module samples =
+    // The fetch functions are only used for showing the time difference
+    // between running requests sequentially vs in parallel, they do not
+    // return anything useful.
     let fetchUrl url =
         let req = WebRequest.Create(Uri(url))
         use resp = req.GetResponse()
